94d1b37dd0b83bd551734aa76c52bf40152b9098
Diego Calciolari Gordiano <diego.gordiano@acad.espm.br>
2024-06-11 18:23:36 -0300

    Script para inserir produtos ao inicializar

--- nest-app/src/main.ts
+++ nest-app/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaClient } from '@prisma/client';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -7,6 +8,19 @@ async function bootstrap() {
   app.enableCors({
     origin: 'http://localhost:3001',
   });
+
+  const prisma = new PrismaClient();
+    const produtos = [
+      { nome: 'Produto 1', tamanho: 'M', valor: 19.99, estoque: 100 },
+      { nome: 'Produto 2', tamanho: 'L', valor: 29.99, estoque: 200 },
+      { nome: 'Produto 3', tamanho: 'S', valor: 9.99, estoque: 300 },
+    ];
+  
+    for (const produto of produtos) {
+      await prisma.produto.create({
+        data: produto,
+      });
+    }
 
   await app.listen(3000);
 }
